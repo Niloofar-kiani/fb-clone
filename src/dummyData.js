59cf1695e3e256bb49de3741d2943372ab20e789
Niloofar-kiani <niloofar.kiani@gmail.com>
2022-10-22 17:06:17 +0400

    updated

---
+++ src/dummyData.js
@@ -0,0 +1,22 @@
+export const Users = [
+    {
+      "id": 1,
+      "profilePicture": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "username": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      "id": 2,
+      "profilePicture": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "username": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      "id": 3,
+      "profilePicture": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "username": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      "id": 4,
+      "profilePicture": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "username": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+]
