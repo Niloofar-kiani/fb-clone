36248a2f97390cc0c3348bb860d448b89e7613ca
kookie579 <roza.asatryan.0@gmail.com>
2022-10-22 18:12:26 +0400

    Profile page updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import Home from "./pages/home/Home";
 import Profile from "./components/profile/Profile";
+import TopBar from "./components/topbar/TopBar";
 
 function App() {
-  return <Profile />;
+  return (
+    <>
+      <TopBar />
+      <Profile />
+    </>
+  );
 }
 
 export default App;
