d49a4890adb4e93543f1344f8d15e95b8ffeee3f
Niloofar-kiani <niloofar.kiani@gmail.com>
2022-10-22 17:33:38 +0400

    update

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,22 +1,58 @@
 export const Users = [
     {
       "id": 1,
-      "profilePicture": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      "username": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+      "profilePicture": "assets/person/image1.jpeg",
+      "username": "Roza Asatryan"
     },
     {
       "id": 2,
-      "profilePicture": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      "username": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+      "profilePicture": "assets/person/image2.jpeg",
+      "username": "Gagik Hovsepyan"
     },
     {
       "id": 3,
-      "profilePicture": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      "username": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+      "profilePicture": "assets/person/image3.jpeg",
+      "username": "Hrach Muradyan"
     },
     {
       "id": 4,
-      "profilePicture": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      "username": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+      "profilePicture": "assets/person/image4.jpeg",
+      "username": "Garik Avetisyan"
     },
+];
+export const Posts =[
+  {
+    id: 1,
+    desc: "Love For All, Hatred For None.",
+    photo: "assets/post/01.jpeg",
+    date: "5 mins ago",
+    userId: 1,
+    like: 32,
+    comment: 9,
+  },
+  {
+    id: 2,
+    photo: "assets/post/02.jpeg",
+    date: "15 mins ago",
+    userId: 2,
+    like: 2,
+    comment: 1,
+  },
+  {
+    id: 3,
+    desc: "Every moment is a fresh beginning.",
+    photo: "assets/post/03.jpeg",
+    date: "1 hour ago",
+    userId: 3,
+    like: 61,
+    comment: 2,
+  },
+  {
+    id: 4,
+    photo: "assets/post/04.jpeg",
+    date: "4 hours ago",
+    userId: 4,
+    like: 7,
+    comment: 3,
+  },
 ]
